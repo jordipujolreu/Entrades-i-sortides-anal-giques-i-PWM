6acbf38369ede9884109b8c610cd086bb575a497
jordipujolreu <jordipujolreu004@gmail.com>
2017-05-01 20:40:58 +0200

    d

---
+++ REPTE 1/_2/_2.ino
@@ -0,0 +1,135 @@
+/**********************************************************************************
+**                                                                               **
+**                   Entrades i sorties analogiques                              **
+**                          Nom del programa                                     **
+**  NOM : Jordi Pujlreu Sala                                   DATA: 1/5/17      **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+const int led0 = 5;          //  adjudiquem nom a la sortida
+const int led1 = 6;          // 
+const int led2 = 3;          // 
+const int led3 = 11;          // 
+const int led4 = 9;         // 
+const int led5 = 10;         // 
+int velocitat = 250;         // velocitat de canvi de DC
+
+//********** Setup ****************************************************************
+void setup()
+{
+  pinMode(led0, OUTPUT);     // definir el pin 3 com una sortida
+  pinMode(led1, OUTPUT);     
+  pinMode(led2, OUTPUT);      
+  pinMode(led3, OUTPUT);     
+  pinMode(led4, OUTPUT);     
+  pinMode(led5, OUTPUT);     
+}
+
+//********** Loop *****************************************************************
+void loop()
+{
+  analogWrite(led0, 0);     // Tots a 0 DC
+  analogWrite(led1, 0);     
+  analogWrite(led2, 0);    
+  analogWrite(led3, 0);     
+  analogWrite(led4, 0);    
+  analogWrite(led5, 0);     
+  
+  delay(velocitat);          // ESPEREM...
+  
+  analogWrite(led0, 60);    // DC a 60 nomes el primer
+  analogWrite(led1, 0);     
+  analogWrite(led2, 0);    
+  analogWrite(led3, 0);    
+  analogWrite(led4, 0);    
+  analogWrite(led5, 0);    
+  
+  delay(velocitat);           // ESPEREM...
+  
+  analogWrite(led0, 120);    // DC a 120 el primer i el segon a 60
+  analogWrite(led1, 60);    
+  analogWrite(led2, 0);    
+  analogWrite(led3, 0);    
+  analogWrite(led4, 0);  
+  analogWrite(led5, 0);    
+   
+  delay(velocitat);           // ESPEREM...
+  
+  analogWrite(led0, 180);    // Seguim amb la sequencia explicada anteriorment
+  analogWrite(led1, 120);    
+  analogWrite(led2, 60);    
+  analogWrite(led3, 0);    
+  analogWrite(led4, 0);    
+  analogWrite(led5, 0);    
+  
+  delay(velocitat);           // es queden leds velocitat ms en aquest estat
+
+  analogWrite(led0, 255);    // Seguim amb la sequencia explicada anteriorment
+  analogWrite(led1, 180);    
+  analogWrite(led2, 120);    
+  analogWrite(led3, 60);    
+  analogWrite(led4, 0);    
+  analogWrite(led5, 0);    
+  
+  delay(velocitat);           // es queden leds velocitat ms en aquest estat
+  
+  analogWrite(led0, 255);    // Seguim amb la sequencia explicada anteriorment
+  analogWrite(led1, 255);    
+  analogWrite(led2, 180);    
+  analogWrite(led3, 120);    
+  analogWrite(led4, 60);    
+  analogWrite(led5, 0);    
+  
+  delay(velocitat);           // es queden leds velocitat ms en aquest estat
+
+  analogWrite(led0, 255);    // Seguim amb la sequencia explicada anteriorment
+  analogWrite(led1, 255);    
+  analogWrite(led2, 180);    
+  analogWrite(led3, 120);    
+  analogWrite(led4, 60);    
+  analogWrite(led5, 0);    
+  
+  delay(velocitat);           // es queden leds velocitat ms en aquest estat
+
+  analogWrite(led0, 255);    // Seguim amb la sequencia explicada anteriorment
+  analogWrite(led1, 255);    
+  analogWrite(led2, 255);    
+  analogWrite(led3, 180);    
+  analogWrite(led4, 120);    
+  analogWrite(led5, 60);    
+  
+  delay(velocitat);           // es queden leds velocitat ms en aquest estat
+
+ analogWrite(led0, 255);    // Seguim amb la sequencia explicada anteriorment
+ analogWrite(led1, 255);    
+ analogWrite(led2, 255);    
+ analogWrite(led3, 255);    
+ analogWrite(led4, 180);    
+ analogWrite(led5, 120);    
+  
+ delay(velocitat);           // es queden leds velocitat ms en aquest estat
+
+ analogWrite(led0, 255);    // Seguim amb la sequencia explicada anteriorment
+ analogWrite(led1, 255);    
+ analogWrite(led2, 255);    
+ analogWrite(led3, 255);    
+ analogWrite(led4, 255);    
+ analogWrite(led5, 180);    
+  
+ delay(velocitat);           // es queden leds velocitat ms en aquest estat
+
+ analogWrite(led0, 255);    // Seguim amb la sequencia explicada anteriorment
+ analogWrite(led1, 255);    
+ analogWrite(led2, 255);    
+ analogWrite(led3, 255);    
+ analogWrite(led4, 255);    
+ analogWrite(led5, 255);    
+  
+ delay(velocitat);           // es queden leds velocitat ms en aquest estat
+}
+
+
+//********** Funcions *************************************************************
+
